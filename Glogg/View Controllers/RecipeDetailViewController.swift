b0944b891f54a310648d0ab579a9fa26072de9e6
Muhammad Murtaza <muhammadmurtaza10@gmail.com>
2020-12-02 01:11:17 +0500

    Add Recipe Detail View Controller, Add custom tab views in recipe detail

---
+++ Glogg/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeDetailViewController.swift
+//  Glogg
+//
+//  Created by Murtaza on 02/12/2020.
+//  Copyright © 2020 Murtaza. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var ingredientsTabView: UIView!
+    @IBOutlet weak var lblIngredients: UILabel!
+    @IBOutlet weak var ingredientsSeperator: UIView!
+    @IBOutlet weak var recipeTabView: UIView!
+    @IBOutlet weak var lblRecipe: UILabel!
+    @IBOutlet weak var recipeSeperator: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
