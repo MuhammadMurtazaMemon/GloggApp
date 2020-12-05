073a8a0a96e302d87284d6368e1b9aaef81bfb40
Rahat Ilyas <rahat_ilyas@outlook.com>
2020-12-05 12:41:23 -0800

    Rahat- Recipe detail screen UI  (Not Finished)

---
+++ Glogg/Cells/IngredientsDetailTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  IngredientsDetailTableViewCell.swift
+//  Glogg
+//
+//  Created by rahat on 05/12/2020.
+//  Copyright © 2020 Murtaza. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblIngredient: UILabel!
+    @IBOutlet weak var lblQuantity: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Glogg/View Controllers/RecipeDetailViewController.swift
+++ Glogg/View Controllers/RecipeDetailViewController.swift
@@ -17,10 +17,34 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var lblRecipe: UILabel!
     @IBOutlet weak var recipeSeperator: UIView!
     
+    @IBOutlet weak var ingredientsDetailTableView: UITableView!
+    
+    var ingredients = ["Beef", "Milk", "Sweet Potatoes", "Curd", "Salt", "All Spices"]
+    var quantity = ["1 Kilo", "330 ml", "500 g", "2 st", "3 g", "15 ml"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews(){
+        let cellNib = UINib(nibName: "IngredientsDetailTableViewCell", bundle: nil)
+        ingredientsDetailTableView.register(cellNib, forCellReuseIdentifier: "IngredientsDetailTableViewCell")
+        
+        
+    }
+}
+extension RecipeDetailViewController :UITableViewDelegate, UITableViewDataSource{
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredients.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientsDetailTableViewCell", for: indexPath) as! IngredientsDetailTableViewCell
+            cell.lblIngredient.text = ingredients[indexPath.row]
+            cell.lblQuantity.text = quantity[indexPath.row]
+            return cell
     }
     
 }
+    
+
